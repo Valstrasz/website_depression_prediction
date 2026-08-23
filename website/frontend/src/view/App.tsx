528679e0bd6973228983533d1ea9408a682f94ae
Valstrasz <grave.valentin@outlook.fr>
2026-08-23 20:02:43 +0200

    Before revamping project to contain only 1 database

--- website/frontend/src/view/App.tsx
+++ website/frontend/src/view/App.tsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+
+import reactLogo from "../asset/react.svg";
+import viteLogo from "../asset/vite.svg";
+import heroImg from "../asset/hero.png";
+
+import "../style/App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -25,7 +27,7 @@ function App() {
         </button>
       </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
